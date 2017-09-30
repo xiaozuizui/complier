59d2935cedf9695c1c5138e6b2d35f941ece6ab5
xiaozuizui <785897146@qq.com>
2017-09-30 17:30:38 +0800

    test

---
+++ complier/DisPoseLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complier
+{
+    class DisPoseLine
+    {
+        public string Line_Str { get; set; }
+        
+        public DisPoseLine(string str)
+        {
+            Line_Str = str;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        private bool IsAlpha(char c)
+        {
+
+        }
+
+        private void Reco(char x)
+        {
+            
+        }
+    }
+}
---
+++ complier/KeyWord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complier
+{
+    public class KeyWord
+    {
+        public string [] Keyword { get; set; }
+
+        public KeyWord()
+        {
+            Keyword[0] = "";
+            Keyword[1] = "integer";
+            Keyword[2] = "true";
+            Keyword[3] = "false";
+            Keyword[4] = "if";
+            Keyword[5] = "else";
+            Keyword[6] = "for";
+            Keyword[7] = "bool";
+            Keyword[8] = "+";
+        }
+    }
+}
