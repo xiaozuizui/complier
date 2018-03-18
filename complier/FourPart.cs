fa924193a8f7e73401b8ac58a81741c8e0d8621e
littlemm <785897146@qq.com>
2018-03-18 16:44:06 +0800

    `

--- complier/FourPart.cs
+++ complier/FourPart.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace complier
 {
-    class FourPart
+    public class FourPart
     {
+        
+            public string Op { get; set; }
+            public string StrLeft { get; set; }
+            public string StrRight { get; set; }
+            public string JumpNum { get; set; }
+
+            public bool Input { get; set; }
+            public FourPart() { }
+
+            public FourPart(string op, string strLeft, string strRight, string jumpNum)
+            {
+                this.Op = op;
+                this.StrLeft = strLeft;
+                this.StrRight = strRight;
+                this.JumpNum = jumpNum;
+                this.Input = false;
+            }
     }
 }
+
--- complier/Semantic .cs
+++ complier/Semantic .cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using complier;
+using complier.Base;
 
 namespace complier
 {
-    class Semantic
+    public class Semantic
     {
+        public List<FourPart> fourparts = new List<FourPart>();
+        public List<Error> errors = new List<Error>();
+        List<Token> tokens;
+      //  public List<symble> symbles = new List<symble>();
+        public string error = "";
+        int i = 0;
+        int ti = 1;
+        string tt = "";
+
+        public Semantic(DisPoseLine m)
+        {
+            tokens = m.tokens;
+            //symbles = m.symbles;
+            //Dispose();
+        }
+
+        private void Next()
+        {
+            if (i < tokens.Count - 1)
+            {
+                i++;
+            }
+        }
+
+        private void Before()
+        {
+            if (i > 0)
+            {
+                i--;
+            }
+        }
+
+        private string NewTemp()
+        {
+            string temp = "T" + ti.ToString();
+            ti++;
+            //symble s = new symble();
+            //s.Name = temp;
+            //symbles.Add(s);
+            return temp;
+        }
+ 
+        private void BackPatch(int addr, int addr2)
+        {
+           // fps[addr].JumpNum = addr2.ToString();
+        }
+       
+        private void Emit(string op, string strLeft, string strRight, string jumpNum)
+        {
+            FourPart fp = new FourPart(op, strLeft, strRight, jumpNum);
+         //   fps.Add(fp);
+        }
     }
 }
